425cc05c681cdaa6f74e5d05f92f5ee87e521d50
Carlos <carlosaac232001@gmail.com>
2026-03-19 17:08:45 -0500

    feat: create prisma client

---
+++ lib/prisma.ts
@@ -0,0 +1,11 @@
+import { configDotenv } from 'dotenv';
+configDotenv({ path: '.env.local' });
+
+import { PrismaPg } from '@prisma/adapter-pg';
+
+import { PrismaClient } from '../generated/prisma/client.js';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+export const prisma = new PrismaClient({ adapter });
