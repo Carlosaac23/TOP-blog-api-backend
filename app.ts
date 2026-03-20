0ad5c43514082308fc64c68c8ba40eb867044794
Carlos <carlosaac232001@gmail.com>
2026-03-19 19:52:17 -0500

    chore: import new post routes

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ configDotenv({ path: '.env.local' });
 
 import express from 'express';
 
+import { postRoutes } from './routes/postRoute.js';
 import { publicRoutes } from './routes/publicRoute.js';
 import { userRoutes } from './routes/userRoute.js';
 import { writerRoutes } from './routes/writerRoute.js';
@@ -12,9 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/', publicRoutes);
-app.use('/api/users/', userRoutes);
-app.use('/api/writers/', writerRoutes);
+app.use('/api', publicRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/writers', writerRoutes);
+app.use('/api/posts', postRoutes);
 
 app.listen(process.env.PORT, error => {
   if (error) {
