50209e647e49c5a2672c307515252ab53e27a150
Carlos <carlosaac232001@gmail.com>
2026-03-25 18:41:16 -0500

    chore: update post schemas to prevent errors

--- schemas/postSchema.ts
+++ schemas/postSchema.ts
@@ -3,14 +3,13 @@ import z from 'zod';
 export const CreatePostSchema = z.object({
   title: z.string(),
   content: z.string(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-
-  writerId: z.cuid2(),
 });
+
+export const UpdatePostSchema = CreatePostSchema.partial();
 
 export const PostSchema = CreatePostSchema.extend({
   id: z.cuid2(),
+  writerId: z.cuid2(),
 });
 
 export type Post = z.infer<typeof PostSchema>;
