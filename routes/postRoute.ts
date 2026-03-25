48df93d79db5fa00a30a216be459b42506050117
Carlos <carlosaac232001@gmail.com>
2026-03-25 17:32:55 -0500

    chore: import new routes

--- routes/postRoute.ts
+++ routes/postRoute.ts
@@ -1,3 +1,16 @@
 import { Router } from 'express';
 
+import { createPost, getPosts, getPost, createComment } from '@/controllers/postController.js';
+import requireRole from '@/middleware/requireRole.js';
+import verifyToken from '@/middleware/verifyToken.js';
+
 export const postRoutes: Router = Router();
+
+postRoutes
+  .route('/')
+  .get(verifyToken, requireRole('user', 'writer'), getPosts)
+  .post(verifyToken, requireRole('writer'), createPost);
+postRoutes
+  .route('/:postId')
+  .get(verifyToken, requireRole('user', 'writer'), getPost)
+  .post(verifyToken, requireRole('user', 'writer'), createComment);
