de919dc8e6ba3f0ff990ed920e6a0fbd8bda3e91
Carlos <carlosaac232001@gmail.com>
2026-03-25 17:35:26 -0500

    chore: use new imports and add login routes

--- app.ts
+++ app.ts
@@ -3,26 +3,30 @@ configDotenv({ path: '.env.local' });
 
 import express from 'express';
 
-import { postRoutes } from './routes/postRoute.js';
-import { publicRoutes } from './routes/publicRoute.js';
-import { userRoutes } from './routes/userRoute.js';
-import { writerRoutes } from './routes/writerRoute.js';
+import passport from '@/config/passport.js';
+import { authRoutes } from '@/routes/authRoute.js';
+import { postRoutes } from '@/routes/postRoute.js';
+import { publicRoutes } from '@/routes/publicRoute.js';
+import { userRoutes } from '@/routes/userRoute.js';
+import { writerRoutes } from '@/routes/writerRoute.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
 
 app.use('/api', publicRoutes);
+app.use('/api/login', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/writers', writerRoutes);
 app.use('/api/posts', postRoutes);
 
-app.listen(process.env.PORT, error => {
+app.listen(process.env['PORT'], error => {
   if (error) {
     console.error(error);
     throw error;
   }
 
-  console.log(`Server started at http://localhost:${process.env.PORT}`);
+  console.log(`Server started at http://localhost:${process.env['PORT']}`);
 });
