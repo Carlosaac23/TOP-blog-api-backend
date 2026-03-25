2c0dacc04791a93a69f0331e68f8d6263a4805be
Carlos <carlosaac232001@gmail.com>
2026-03-25 17:31:58 -0500

    feat: create middleware to verify if current user is a user or writer

---
+++ middleware/requireRole.ts
@@ -0,0 +1,19 @@
+import type { Request, Response, NextFunction } from 'express';
+
+import type { AuthRole } from '@/types/index.js';
+
+export default function requireRole(...allowedRoles: AuthRole[]) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const role = req.user?.role;
+
+    if (!role) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    if (!allowedRoles.includes(role)) {
+      return res.status(403).json({ message: 'Forbidden' });
+    }
+
+    next();
+  };
+}
