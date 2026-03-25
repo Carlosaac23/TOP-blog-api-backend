92a991fec9d1d342d46ae368934979b8bfdbb03a
Carlos <carlosaac232001@gmail.com>
2026-03-25 17:32:21 -0500

    feat: create middleware to verify current token

---
+++ middleware/verifyToken.ts
@@ -0,0 +1,29 @@
+import { configDotenv } from 'dotenv';
+configDotenv({ path: '.env.local' });
+
+import type { Request, Response, NextFunction } from 'express';
+
+import jwt from 'jsonwebtoken';
+
+import type { AuthRole } from '@/types/index.js';
+
+export default function verifyToken(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(' ')[1];
+  console.log('Token:', token);
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env['JWT_SECRET'] as string);
+    req.user = decoded as { sub: string; role: AuthRole };
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+}
