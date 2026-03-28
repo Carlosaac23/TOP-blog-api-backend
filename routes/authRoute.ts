1e43124d8f405c273a98e449a5826fa670911692
Carlos <carlosaac232001@gmail.com>
2026-03-27 22:00:39 -0500

    chore: create route to get writer/user profile

--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 
 import { login } from '@/controllers/authController.js';
+import { getUser } from '@/controllers/userController.js';
+import verifyToken from '@/middleware/verifyToken.js';
 
 export const authRoutes: Router = Router();
 
 authRoutes.post('/', login);
+authRoutes.get('/profile', verifyToken, getUser);
