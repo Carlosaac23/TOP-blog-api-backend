aa5fb3fecb6805cd84e86f34df86ea8e45284075
Carlos <carlosaac232001@gmail.com>
2026-03-27 22:01:24 -0500

    chore: update routes with middleware

--- routes/userRoute.ts
+++ routes/userRoute.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import passport from 'passport';
 
-import { createUser, updateUser, deleteUser, getUsers } from '@/controllers/userController.js';
+import { createUser, getUsers, updateUser, deleteUser } from '@/controllers/userController.js';
+import verifyToken from '@/middleware/verifyToken.js';
 
 export const userRoutes: Router = Router();
 
 userRoutes.route('/').get(getUsers).post(createUser);
-userRoutes.put('/:userId', passport.authenticate('jwt', { session: false }), updateUser);
-userRoutes.delete('/:userId', passport.authenticate('jwt', { session: false }), deleteUser);
+userRoutes.put('/:userId', verifyToken, updateUser);
+userRoutes.delete('/:userId', verifyToken, deleteUser);
