cd8a860ea905900e5349ea3c31c840965194b5bf
Carlos <carlosaac232001@gmail.com>
2026-03-30 18:41:48 -0500

    chore: clean auth route

--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,10 +1,7 @@
 import { Router } from 'express';
 
 import { login } from '@/controllers/authController.js';
-import { getUser } from '@/controllers/userController.js';
-import verifyToken from '@/middleware/verifyToken.js';
 
 export const authRoutes: Router = Router();
 
 authRoutes.post('/', login);
-authRoutes.get('/profile', verifyToken, getUser);
