b2f0347497c1f6b60cdd72d44629cf39204be249
Carlos <carlosaac232001@gmail.com>
2026-03-30 18:45:34 -0500

    chore: import new comment routes

--- app.ts
+++ app.ts
@@ -6,9 +6,9 @@ import express from 'express';
 
 import passport from '@/config/passport.js';
 import { authRoutes } from '@/routes/authRoute.js';
+import { commentRoutes } from '@/routes/commentRoute.js';
 import { postRoutes } from '@/routes/postRoute.js';
-import { userRoutes } from '@/routes/userRoute.js';
-import { writerRoutes } from '@/routes/writerRoute.js';
+import { userRoutes, writerRoutes } from '@/routes/subjectRoute.js';
 
 const app = express();
 
@@ -21,6 +21,8 @@ app.use('/api', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/writers', writerRoutes);
 app.use('/api/posts', postRoutes);
+app.use('/api/posts/:postId/comments', commentRoutes);
+app.use('/api/comments', commentRoutes);
 
 app.listen(process.env['PORT'], error => {
   if (error) {
