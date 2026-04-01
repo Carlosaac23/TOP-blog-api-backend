5b8f78566e0be62408695a0cbdbac1c1cf09ed84
Carlos <carlosaac232001@gmail.com>
2026-03-31 23:31:26 -0500

    chore: improve config for cors

--- app.ts
+++ app.ts
@@ -23,6 +23,9 @@ const corsOptions: CorsOptions = {
 
     cb(new Error('Not allowed by CORS'));
   },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
